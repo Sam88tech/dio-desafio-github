2a5197ce24d73bbab9377164a0798662fd578085
samuel <sis88nas@hotmail.com>
2022-07-07 18:18:17 -0300

    nuevos

---
+++ apresentação/python/operadores.py
@@ -0,0 +1,21 @@
+#operadores logicos
+a = 4
+b = 10
+c = 5.0
+d = 1
+f = 5
+
+print (a == c)
+print (a < b)
+print (d > b)
+print (c != f)
+print (a == b)
+print (c < d)
+
+
+a = True
+b = False
+c = True
+print (a and a)
+print (b and b)
+print (not c)
---
+++ apresentação/python/teste.py
@@ -0,0 +1,40 @@
+# print("Samuel ")
+
+a = 2
+b = 3
+
+#print (a+b) #
+
+# Programa cálculo aumento salario
+
+salário = 1500
+aumento = 5
+print("Salário com aumento de 5%:")
+print(salário + (salário * aumento / 100))
+print ("Samuel Silva")
+
+# resultado com (a  & b)
+a = 3
+b = 5
+print ("o resultado é : ")
+print((2*a)*(3*b))
+
+
+# soma 3 variaveis
+num1 = float(input("Digite 1° numero: "))
+num2 = float(input("Digite 2° numero: "))
+num3 = float(input("Digite 3° numero: "))
+resultado = num1 + num2 + num3
+print("A soma de",num1, "e",num2, "e",num3,"é:",resultado)
+
+print ("O valor é :")
+
+
+# Programa cálculo aumento salario
+
+salário = 750
+aumento = 15
+print("Salário com aumento de 15%:")
+print(salário + (salário * aumento / 100))
+
+
